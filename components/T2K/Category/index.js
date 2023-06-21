825bfb637c2ec945237a9880fc5911d9580c9b20
Ahmed <janahmed902@gmail.com>
2023-06-21 14:03:36 +0530

    abcd

--- components/T2K/Category/index.js
+++ components/T2K/Category/index.js
@@ -2,10 +2,21 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faCircleArrowRight
+    faCircleArrowRight,
+    faHotel
 } from "@fortawesome/free-solid-svg-icons";
 import Card from '../Card';
 
+
+import EmojiTransportationIcon from '@mui/icons-material/EmojiTransportation';
+import HomeWorkOutlinedIcon from '@mui/icons-material/HomeWorkOutlined';
+import CottageOutlinedIcon from '@mui/icons-material/CottageOutlined';
+import GiteOutlinedIcon from '@mui/icons-material/GiteOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import ApartmentOutlinedIcon from '@mui/icons-material/ApartmentOutlined';
+import HouseSidingOutlinedIcon from '@mui/icons-material/HouseSidingOutlined';
+import BungalowOutlinedIcon from '@mui/icons-material/BungalowOutlined';
+import HolidayVillageOutlinedIcon from '@mui/icons-material/HolidayVillageOutlined';
 
 function Category() {
 
@@ -30,12 +41,25 @@ function Category() {
             })
     }
 
+
+    let icons = {
+        "hotel": <ApartmentOutlinedIcon sx={{ fontSize: '60px', color: '#2912d3' }} />,
+        "resort": <HouseSidingOutlinedIcon sx={{ fontSize: '60px', color: '#6BB477 ' }} />,
+        "motel": <BungalowOutlinedIcon sx={{ fontSize: '60px', color: '#B69716' }} />,
+        "home stay": <HomeOutlinedIcon sx={{ fontSize: '60px', color: '#2912d3' }} />,
+        "guest house": <HomeWorkOutlinedIcon sx={{ fontSize: '60px', color: '#2912d3' }} />,
+        "lodge": <CottageOutlinedIcon sx={{ fontSize: '60px', color: '#B69716 ' }} />,
+        "hostel": <GiteOutlinedIcon sx={{ fontSize: '60px', color: '#2912d3' }} />,
+        "hut": <HolidayVillageOutlinedIcon sx={{ fontSize: '60px', color: '#6BB477' }} />,
+    }
+
+
     return (
         <section>
             <div className='py-10'>
                 <div className='py-8'>
-                    <div className='px-3'>
-                        <div className='text-center'>
+                    <div className='px-3 text-center'>
+                        <div className='text-center mb-6'>
                             <span className='bg-blue-100 pr-2 py-2 rounded-2xl'>
                                 <span><FontAwesomeIcon icon={faCircleArrowRight} size="2xl" style={{ color: "#2912d3", }} /></span>
                                 <span className='text-blue-800 text-xl font-medium pl-3 tracking-wide'>Category</span>
@@ -43,7 +67,20 @@ function Category() {
                             <h2 className='mt-4 mb-6 text-3xl font-medium text-gray-700'>Select From Our Categories</h2>
                             <p className='text-gray-500 tracking-wide font-medium'>Choose from luxurious huts, cozy hotel rooms, beautiful home stays, wonderful lodges, or serene countryside resort estates for your perfect vacation.</p>
                         </div>
-                        <Card />
+                        <div className='md:flex md:flex-wrap md:gap-10 md:justify-center'>
+                            {allPropertyTypes.map((item, index) =>
+                                <div className='md:w-5/12  lg:w-3/12'>
+                                    <Card
+                                        key={index}
+                                        propertyIcon={icons[item?.property_type]}
+                                        propertyType={item?.property_type}
+                                        propertyDescription={item?.description}
+                                    />
+                                </div>
+
+                            )}
+                        </div>
+
 
                     </div>
 
