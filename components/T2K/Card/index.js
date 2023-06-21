a332dab53750e80110cf1aae0f52a07012012c22
Ahmed <janahmed902@gmail.com>
2023-06-21 14:02:59 +0530

    added categories and card components

--- components/T2K/Card/index.js
+++ components/T2K/Card/index.js
@@ -2,25 +2,34 @@ import React from 'react';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faCarBuilding
+  faArrowRight,
+  faHotel
 } from "@fortawesome/free-solid-svg-icons";
+import EmojiTransportationIcon from '@mui/icons-material/EmojiTransportation';
+import HomeWorkOutlinedIcon from '@mui/icons-material/HomeWorkOutlined';
+import CottageOutlinedIcon from '@mui/icons-material/CottageOutlined';
+import GiteOutlinedIcon from '@mui/icons-material/GiteOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import ApartmentOutlinedIcon from '@mui/icons-material/ApartmentOutlined';
+import HouseSidingOutlinedIcon from '@mui/icons-material/HouseSidingOutlined';
 
 
 
-function Card({propertyIcon,propertyType,propertyDescription}) {
+function Card({ propertyIcon, propertyType, propertyDescription }) {
+
   return (
-    <div className='py-12'>
-        <div>
-            <div className='p-8'>
-                {/* <span>{propertyIcon}</span> */}
-                <span>
-                <FontAwesomeIcon icon={faCarBuilding} size="2xl" style={{color: "#446aab",}} />
-                </span>
-                {/* <h5>{propertyType}</h5> */}
-                {/* <p>{propertyDescription}</p> */}
-            </div>
-
+    <div className='border rounded-xl md:h-full  md:hover:shadow-2xl'>
+      <div className='pb-8'>
+        <div className='p-8'>
+          <span>{propertyIcon}</span>
+          <h5 className='text-xl font-semibold text-gray-700 mt-5 mb-2 capitalize'>{propertyType}</h5>
+          <p className='text-gray-500 tracking-wide font-normal'>{propertyDescription?.substring(0,100)}</p>
         </div>
+        <div>
+          <a className='p-9 font-semibold' style={{ color: "#2912d3", }}>Read More &nbsp;<FontAwesomeIcon icon={faArrowRight} /> </a>
+        </div>
+
+      </div>
     </div>
   )
 }
