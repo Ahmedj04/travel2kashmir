85c0fd9577072cd2c11088b272212e082f226b8c
Ahmed <janahmed902@gmail.com>
2023-06-21 15:42:22 +0530

    modified header for desktop view

--- components/T2K/Header/index.js
+++ components/T2K/Header/index.js
@@ -6,9 +6,19 @@ import MenuIcon from '@mui/icons-material/Menu';
 function Header() {
   return (
     <section className='bg-white'>
-        <div className='py-3 px-2 shadow shadow-lg flex justify-between'>
-            <i><AccountCircleIcon fontSize='large'/></i>
-            <i><MenuIcon fontSize='large'/></i>
+        <div className='py-3 lg:py-5 px-2 lg:px-5 shadow shadow-lg flex justify-between'>
+            {/* mobile view  and tablet view */}
+            <i className='lg:hidden'><AccountCircleIcon fontSize='large'/></i>
+            <i className='lg:hidden'><MenuIcon fontSize='large'/></i>
+
+            {/* desktop view */}
+            <ul className='hidden lg:block lg:flex lg:gap-10'>
+              <li className='ml-32 text-base font-medium text-gray-700 hover:text-blue-600 cursor-pointer'><a href='/'>Home</a></li>
+              <li className='font-medium text-gray-700 hover:text-blue-600 hover:text-blue-800 cursor-pointer'>Listings</li>
+              
+            </ul>
+            <i className='hidden lg:block'><AccountCircleIcon fontSize='large'/></i>
+
         </div>
     </section>
   )
